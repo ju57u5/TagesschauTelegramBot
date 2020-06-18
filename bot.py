a148e40f456ca1850c096a38820128205358cc24
Ju57u5 <2545457+ju57u5@users.noreply.github.com>
2020-06-18 23:14:11 +0200

    fix, refactor: Make Pylint rate this code 10/10

--- bot.py
+++ bot.py
@@ -1,33 +1,40 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler, PicklePersistence
-from telegram.ext.filters import Filters
-from telegram import InlineQueryResultVideo, InputTextMessageContent, ParseMode
-import requests
+"""
+TagesschauTelegramBot
+
+Share the current episode of Tagesschau/Tagesthemen with a quick inline command.
+"""
 import logging
 import json
+import requests
+from telegram.ext import Updater, InlineQueryHandler, PicklePersistence
+from telegram import InlineQueryResultVideo, InputTextMessageContent, ParseMode
 from lxml import etree
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 CACHE_TIME = 10
-bot_persistence = PicklePersistence(filename='bot_data', store_bot_data=True)
-
 
 """
 Configuration for the different show. Format:
 {
     "keywords": List of all possible keywords, this this show should be found with.
 
-    "quality": Dict of feeds for the different quality versions of this show. key: quality-identifier, value: feed-url.
+    "quality": Dict of feeds for the different quality versions of this show. key:
+               quality-identifier, value: feed-url.
 
-    "default_quality": String with the default quality-indentifiert, that should be chosen if one isnt specified. 
-                       This should preferably be a feed, that produces files < 20MB so that they can be shown in the video player.
+    "default_quality": String with the default quality-indentifiert, that should be
+                       chosen if one isnt specified. This should preferably be a feed,
+                       that produces files < 20MB so that they can be shown in the video player.
 
-    "input_message_content": If the video result should use input_message_content to just print the url. 
-                             This should be set to true, if no feed can get under filesize < 20MB to allow basic functionality wihtout errors. 
+    "input_message_content": If the video result should use input_message_content to just print
+                             the url.
+                             This should be set to true, if no feed can get under filesize < 20MB to
+                             allow basic functionality wihtout errors.
 
 }
 """
+# pylint: disable=C0301
 SHOW_CONFIG = [
     {
         "keywords": ["all", "tagesschau", "schau"],
@@ -67,15 +74,21 @@ SHOW_CONFIG = [
     },
 ]
 
-with open("credentials.json") as config:
-    key = json.load(config)["key"]
-    updater = Updater(key, persistence=bot_persistence, use_context=True)
-
 
 def get_newest_episode_from_podcast_feed(podcast_url, input_message_content=False):
+    """
+    Get the newest episode from the podcast feed and return a list of InlineQueryResultVideos
+    with the only member containing that episode.
+
+    Keyword arguments:
+    podcast_url (str): URL of the podcast where the episode should be taken from. The podcast must order its items from
+                       most recent to oldest, because the first item is picked without any time comparisons.
+    input_message_content (bool): Set the optional parameter input_message_content if the result should only contain a link.
+                                  This is important if the podcast file is bigger than 20 MB because the response will fail.
+    """
     rss_response = requests.get(podcast_url)
     root = etree.fromstring(rss_response.content)
-    
+
     channel = root.find("channel")
     item = channel.find("item")
     title = item.find("title").text
@@ -86,24 +99,27 @@ def get_newest_episode_from_podcast_feed(podcast_url, input_message_content=Fals
     enclosure = item.find("enclosure")
     url = enclosure.attrib["url"]
     mime_type = enclosure.attrib["type"]
-    
-    
+
     if not input_message_content:
         return [InlineQueryResultVideo(id=uid, video_url=url, mime_type=mime_type, thumb_url=thumb, title=title, caption=description, description=description)]
-    else:
-        content = InputTextMessageContent(
-            f"{url}", parse_mode=ParseMode.MARKDOWN)
-        return [InlineQueryResultVideo(input_message_content=content, id=uid, video_url=url, mime_type=mime_type, thumb_url=thumb, title=title, caption=description, description=description)]
+
+    content = InputTextMessageContent(f"{url}", parse_mode=ParseMode.MARKDOWN)
+    return [InlineQueryResultVideo(input_message_content=content, id=uid, video_url=url, mime_type=mime_type, thumb_url=thumb, title=title, caption=description, description=description)]
 
 
 def inline_query_handler(update, context):
+    """
+    Inline query handler for the bot. Returns the appropriate shows as a result.
+
+    query_text must be in the space seperated argument form: <keyword> <quality> <send-link>. Check the README for more information.
+    """
     query_text = update.inline_query.query
     query_id = update.inline_query.id
 
     if not query_text:
         query_text = "all"
 
-    logging.info(f"Query {query_text}")
+    logging.info("Query %s", query_text)
     query_text = query_text.lower()
     query_args = query_text.split()
 
@@ -124,7 +140,7 @@ def inline_query_handler(update, context):
                 default_quality = show["default_quality"]
                 feed = show["quality"][default_quality]
 
-            if input_message_content_parameter != None:
+            if input_message_content_parameter is not None:
                 input_message_content = input_message_content_parameter.lower() in [
                     "true", "1", "yes"]
             else:
@@ -139,6 +155,13 @@ def inline_query_handler(update, context):
 
 
 if __name__ == "__main__":
+    with open("credentials.json") as config:
+        key = json.load(config)["key"]
+
+    bot_persistence = PicklePersistence(
+        filename='bot_data', store_bot_data=True)
+    updater = Updater(key, persistence=bot_persistence, use_context=True)
+
     updater.dispatcher.add_handler(InlineQueryHandler(inline_query_handler))
     updater.start_polling()
     updater.idle()
